f17a6e7ab185175d5ba7b79ce0c70371daaa6fc8
zhangyehui <zhangyehui@u51.com>
2017-10-25 12:26:26 +0800

    test push

---
+++ src/main/java/org/apache/ibatis/datasource/hpooled/HPooledDataSourceFactory.java
@@ -0,0 +1,12 @@
+package org.apache.ibatis.datasource.hpooled;
+
+import org.apache.ibatis.datasource.unpooled.UnpooledDataSourceFactory;
+
+/**
+ * Created by zhangyehui on 2017/10/25.
+ */
+public class HPooledDataSourceFactory extends UnpooledDataSourceFactory {
+
+    public HPooledDataSourceFactory() {
+    }
+}
